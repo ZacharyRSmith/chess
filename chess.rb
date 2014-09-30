438ae076f17ee249165e6abd67e34708eb2a4c33
Zachary R. Smith <ZacharySmith89@yahoo.com>
2014-09-30 15:07:46 -0400

    code #los for Queen

--- chess.rb
+++ chess.rb
@@ -418,6 +418,13 @@ class Queen < Piece
   def initialize(square, owner, moved = false)
     super(square, owner, moved)
     @icon = "Q"
+    @los = self.los()
+  end
+  
+  def los
+    los = build_bishop_los()
+    los = los + build_rook_los()
+    los
   end
 end
 
