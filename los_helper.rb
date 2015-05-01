daf5a26472f67f04f00b9117645618f15fbec4ff
ZacharyRSmith <ZacharySmith4989@gmail.com>
2015-05-01 15:31:24 +0000

    Move bishop's los function into its class file.

--- los_helper.rb
+++ los_helper.rb
@@ -1,33 +1,3 @@
-def build_bishop_los
-  x_orig = self.square.coor[0]
-  y_orig = self.square.coor[1]
-  rslt_squares = []
-  ary_1s = [-1, 1]
-
-  for add_x in ary_1s
-    for add_y in ary_1s
-      x_now = x_orig + add_x
-      y_now = y_orig + add_y
-
-      until !x_now.between?(0, 7) || !y_now.between?(0, 7)
-        if $board[x_now][y_now].has() &&
-                                    $board[x_now][y_now].has().owner == $player
-          break
-        end
-
-        rslt_squares << $board[x_now][y_now]
-        if $board[x_now][y_now].has()
-          break
-        end
-        x_now = x_now + add_x
-        y_now = y_now + add_y
-      end
-    end
-  end
-
-  rslt_squares
-end
-
 def build_rook_los
   x_orig = self.square.coor[0]
   y_orig = self.square.coor[1]
--- pieces/bishop.rb
+++ pieces/bishop.rb
@@ -1,5 +1,4 @@
 require_relative '../coor_helper'
-require_relative '../los_helper'
 require_relative 'piece'
 
 class Bishop < Piece
@@ -9,6 +8,32 @@ class Bishop < Piece
   end
 
   def los
-    build_bishop_los()
+    x_orig = self.square.coor[0]
+    y_orig = self.square.coor[1]
+    rslt_squares = []
+    ary_1s = [-1, 1]
+
+    for add_x in ary_1s
+      for add_y in ary_1s
+        x_now = x_orig + add_x
+        y_now = y_orig + add_y
+
+        until !x_now.between?(0, 7) || !y_now.between?(0, 7)
+          if $board[x_now][y_now].has() &&
+                                      $board[x_now][y_now].has().owner == $player
+            break
+          end
+
+          rslt_squares << $board[x_now][y_now]
+          if $board[x_now][y_now].has()
+            break
+          end
+          x_now = x_now + add_x
+          y_now = y_now + add_y
+        end
+      end
+    end
+
+    rslt_squares
   end
 end
