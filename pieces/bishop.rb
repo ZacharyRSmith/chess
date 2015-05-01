d0e6e247cb859d43b3d0dfb9b351d42290e5c534
ZacharyRSmith <ZacharySmith4989@gmail.com>
2015-05-01 15:57:14 +0000

    Move los function into rook's class file, and refactor rook's and bishop's los functions.

--- pieces/bishop.rb
+++ pieces/bishop.rb
@@ -11,21 +11,21 @@ class Bishop < Piece
     x_orig = self.square.coor[0]
     y_orig = self.square.coor[1]
     rslt_squares = []
-    ary_1s = [-1, 1]
 
-    for add_x in ary_1s
-      for add_y in ary_1s
+    for add_x in [-1, 1]
+      for add_y in [-1, 1]
         x_now = x_orig + add_x
         y_now = y_orig + add_y
 
         until !x_now.between?(0, 7) || !y_now.between?(0, 7)
-          if $board[x_now][y_now].has() &&
-                                      $board[x_now][y_now].has().owner == $player
+          crnt_square = $board[x_now][y_now]
+
+          if crnt_square.has() && crnt_square.has().owner == $player
             break
           end
+          rslt_squares << crnt_square
 
-          rslt_squares << $board[x_now][y_now]
-          if $board[x_now][y_now].has()
+          if crnt_square.has()
             break
           end
           x_now = x_now + add_x
--- pieces/rook.rb
+++ pieces/rook.rb
@@ -10,6 +10,44 @@ class Rook < Piece
   end
 
   def los
-		build_rook_los()
+    x_orig = self.square.coor[0]
+    y_orig = self.square.coor[1]
+    rslt_squares = []
+
+    for add_x in [-1, 1]
+      x_now = x_orig + add_x
+      until !x_now.between?(0, 7)
+        crnt_square = $board[x_now][y_orig]
+
+        if crnt_square.has() && crnt_square.has().owner == $player
+          break
+        end
+        rslt_squares << crnt_square
+
+        if crnt_square.has()
+          break
+        end
+        x_now += add_x
+      end
+    end
+
+    for add_y in [-1, 1]
+      y_now = y_orig + add_y
+      until !y_now.between?(0, 7)
+        crnt_square = $board[x_orig][y_now]
+
+        if crnt_square.has() && crnt_square.has().owner == $player
+          break
+        end
+        rslt_squares << crnt_square
+
+        if crnt_square.has()
+          break
+        end
+        y_now += add_y
+      end
+    end
+
+    rslt_squares
   end
 end
