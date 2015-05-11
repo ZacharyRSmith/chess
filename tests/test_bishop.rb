1630fa3c262e7e8116150442c8641c3911581f4c
ZacharyRSmith <ZacharySmith4989@gmail.com>
2015-05-11 20:46:46 +0000

    TestBishop#test_set_los_with_blank_board.

--- tests/test_bishop.rb
+++ tests/test_bishop.rb
@@ -26,6 +26,16 @@ class TestBishop < Minitest::Test
     assert_equal(" ", bishop.owner)
   end
 
-  def test_set_los
+  def test_set_los_with_blank_board
+    bishop = gen_bishop(coordinates: [1,1])
+    bishop.set_los()
+    actual_coords = []
+    bishop.los.each { |sqr| actual_coords << sqr.coordinates }
+
+    expected_coords = [[0,0], [3,3], [0,2], [2,0], [2,2], [4,4], [5,5], [6,6], [7,7]]
+    actual_coords.sort!
+    expected_coords.sort!
+
+    assert_equal(expected_coords, actual_coords)
   end
 end
