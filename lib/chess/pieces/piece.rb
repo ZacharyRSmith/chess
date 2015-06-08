c8b3f2730538c4737b72a4b15949f36f113d606e
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-06-08 20:19:10 +0000

    Add attr_reader for Piece.board and set Piece.icon to 'X'

--- lib/chess/pieces/piece.rb
+++ lib/chess/pieces/piece.rb
@@ -3,7 +3,7 @@ class Piece
     @board     = square.board
     @can_move  = FALSE
     # Every piece should have an @icon, but I am leaving that to child classes.
-    # @icon      = "X"
+    @icon      = "X"
     @in_los_of = []
     @los       = []
     @moved     = FALSE
@@ -12,5 +12,5 @@ class Piece
   end
 
   attr_accessor :can_move, :icon, :in_los_of, :moved, :owner, :square
-  attr_reader   :los
+  attr_reader   :board, :los
 end
