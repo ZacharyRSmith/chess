b2c79423d3488608904d14470201648ae2d8e71f
Zachary R. Smith <ZacharyRSmith@gmail.com>
2015-06-09 00:29:23 +0000

    TestKing

---
+++ tests/test_king.rb
@@ -0,0 +1,53 @@
+require './lib/chess/pieces/king'
+require './lib/chess/pieces/piece'
+require './lib/chess/square'
+require './lib/chess/board'
+require 'minitest/autorun'
+
+def set_up_default_board
+  board = Board.new()
+  board.set_up_pawns()
+  board.set_up_back_rows()
+
+  board
+end
+
+
+class Testking < MiniTest::Test
+  def test_icon
+    board = set_up_default_board()
+    king = board.get_square(4, 0).piece
+
+    assert_equal("K", king.icon)
+  end
+
+  def test_owner_with_comma
+    board = set_up_default_board()
+    king = board.get_square(4, 0).piece
+
+    assert_equal(" ", king.owner)
+  end
+
+  def test_owner_with_blank
+    board = set_up_default_board()
+    king = board.get_square(4, 0).piece
+
+    assert_equal(",", king.owner)
+  end
+
+  def test_cannot_move_when_blocked_by_friendly_pieces
+    board = set_up_default_board()
+    king = board.get_square(4, 0).piece
+
+    refute(king.can_move)
+  end
+
+  def test_can_move_when_blocked_by_enemy_pieces
+    board = set_up_default_board()
+    king = board.get_square(4, 0).piece
+    sqr = board.get_square(4, 1)
+    sqr.piece = Piece.new(owner: ",", square: sqr)
+
+    assert(king.can_move)
+  end
+end
